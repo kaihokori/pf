462c348bbbebc415ddb219b55dbfcb9bbb37eab3
kaihokori <96321538+kaihokori@users.noreply.github.com>
2025-11-30 06:32:54 +1100

    Initial Commit

---
+++ Pump Fitness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pump Fitness
+//
+//  Created by Kyle Graham on 30/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pump Fitness/Pump_FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pump_FitnessApp.swift
+//  Pump Fitness
+//
+//  Created by Kyle Graham on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pump_FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
